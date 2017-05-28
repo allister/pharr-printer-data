c7819aa3a5d98790bc2206868270ff4525c598ae
Rich Gillin <rich@gillin.us>
2017-05-27 17:19:18 -0800

    successful ingest. TODO XML to CSV R Code.

--- ingest.R
+++ ingest.R
@@ -2,16 +2,23 @@
 library(tidyverse)
 # setup XML task view
 library(XML)
+library(scraper)
 # library(xml2)
 # library(XML2R)
 
 ## Data Ingestion
 xmlIngest <- "Permits_Printer-Job_Log.xml"
 printer_data <- xmlParse(xmlIngest)
-printer_dataTree <- xmlInternalTreeParse(xmlIngest) 
+printer_dataTree <- xmlInternalTreeParse(xmlIngest)
 # printer_data <- xmlParseDoc(xmlIngest, asText = TRUE)
 ## creates Class 'XMLInternalDocument' <externalptr>
 printer_data <- xmlTreeParse(xmlIngest)
 ingestedList <- xmlToList(printer_data, addAttributes = T)
 ingestedXML_df <- xmlToDataFrame(printer_data)
 # fails 'duplicate subscripts for columns'
+xmlScraper <- scrape(file = xmlIngest, isXML = T)
+
+# From Zeds CSV
+## Load csv & see what we get
+permitsLog_ingest <- read_csv('Printer_Log-Permits.csv')
+# done ingesting -- need code in R for XML to CSV conversion TODO
